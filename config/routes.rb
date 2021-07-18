f9e65de9b2651320332d7050f5d74909d65a9f89
Your Name <rimzimsharma03@gmail.com>
2021-07-18 12:41:21 +0530

    front-end-designs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'  ### before login
+  resources :home do 		### index routes
+    collection do
+      get "about_us"   ###about_us
+      get "contact_us" ###Contact_us
+      get "cashtelex_exchange" ###services1
+      get "cashtelex_go" ###services2
+      get "cashtelex_coin" ###services3
+      get "cashtelex_real_estate" ###services1
+      get "cashtelex_gold" ###services1
+    end
+  end
 
   devise_for :admins
 
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_07_13_165319) do
 
-  create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
